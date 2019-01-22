576aec249c2f7c79b6fb4816170b2ec3237c4b80
zarrar <zarrarmalik81@gmail.com>
2019-01-22 16:03:59 +0000

    Integration tests with get and post

---
+++ student-services/src/main/java/com/myself/testing/StudentServicesApplication.java
@@ -0,0 +1,14 @@
+package com.myself.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServicesApplication.class, args);
+	}
+
+}
+
